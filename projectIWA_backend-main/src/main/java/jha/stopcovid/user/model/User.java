fe5c969714f0b350428031398f7885c054a780b8
AlexisAndre34 <alexis.andrepro34@gmail.com>
2020-11-15 10:57:33 +0100

    new branch

--- projectIWA_backend-main/src/main/java/jha/stopcovid/user/model/User.java
+++ projectIWA_backend-main/src/main/java/jha/stopcovid/user/model/User.java
@@ -1,7 +1,13 @@
 package jha.stopcovid.user.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import  javax.persistence.Id;
+
+@Entity(name = "users")
 public class User {
 
+    @Id
     private String Id;
 
     private String email;
