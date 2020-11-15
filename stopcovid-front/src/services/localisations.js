6c57b556b8022660f66ce261a8e58d003e2e221d
AlexisAndre34 <alexis.andrepro34@gmail.com>
2020-11-15 09:48:53 +0100

    fix port front

--- stopcovid-front/src/services/localisations.js
+++ stopcovid-front/src/services/localisations.js
@@ -1,7 +1,7 @@
 export const sendLocalisationToApi = (latitude, longitude, timestamp, token) => {
     const data = JSON.stringify({latitude: latitude, longitude: longitude, timestamp: timestamp});
     //const data = {latitude: latitude, longitude: longitude, timestamp: timestamp};
-    return fetch(`http://localhost:3001/publish`,
+    return fetch(`http://localhost:5000/publish`,
         {
             method: 'POST',
             headers: {
